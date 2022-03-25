4fddeaf78c4e496aa00394d538c29b291c35366d
Artem <kvar65@yandex.ru>
2022-03-26 00:12:18 +0300

    последний вариант

--- case.py
+++ case.py
@@ -1,14 +1,10 @@
 import json
 import requests
-import urllib.request
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 
-# FILE_NAME = "case"
 
-def parse(url):
-    # list = {'description': [], 'url': [],
-    #         'price': []}
+def url_parse(url):
     url_list = []
     r = requests.get(url)
     if r.status_code == 200:
@@ -23,46 +19,36 @@ def parse(url):
             url_list.append('https://aliexpress.ru'+href.a['href'])
         return url_list
 
-        # for href in urls:
-        #     urllib.request.urlopen('https://aliexpress.ru'+href.a['href'])
 
-        # describe = soup.find_all(
-        #     'div', class_='product-snippet_ProductSnippet__name__xti7vm'
-        # )[:10]
-        # prices = soup.find_all(
-        #     'div', class_='snow-price_SnowPrice__mainS__18s9w6'
-        # )[:10]
-        # for price in prices:
-        #     list['price'].append(price.text)
-        # for discr in describe:
-        #     list['description'].append(discr.text)
-        # for href in urls:
-        #     url_list['url'].append('https://aliexpress.ru'+href.a['href'])
-        # return url_list
-
-
-    # if r.status_code == 400:
-    #     status_code = {400: 'Bad request, try again!'}
-    #     sc = json.dumps(status_code)
-    #     return sc
-    # if r.status_code == 521:
-    #     status_code = {521: 'Bad request, try again!'}
-    #     sc = json.dumps(status_code)
-    #     return sc
-    # if r.status_code == 204:
-    #     status_code = {204: 'Bad request, try again!'}
-    #     sc = json.dumps(status_code)
-    #     return sc
-
-    for the_url in url_list:
-        list = {'title': [], 'description': [], 'url': [],
-                'price': [], 'pubDate': []}
-        req = requests.get(the_url)
+def parse(url_list):
+    for i in url_list:
+        the_list = {'title': [], 'description': [], 'url': [],
+                    'price': []}
+        req = requests.get(i)
         if req.status_code == 200:
-            with open("test.txt", "w") as f:
+            with open("test2.txt", "w") as f:
                 f.write(f"{req.text}\n")
             soup = bs(req.text, 'html.parser')
-
+            titles = soup.find_all(
+                'div', class_='Product_Name__container__hntp3'
+            )
+            describe = soup.find_all(
+                'div', class_='detail-desc-decorate-richtext'
+            )
+            prices = soup.find_all(
+                'div', class_='Product_Price__container__1uqb8 product-price'
+            )
+                # pubDate = soup.find_all(
+                #     '', class_=''
+                # )
+            for title in titles:
+                the_list['title'].append(title.text)
+            for discr in describe:
+                the_list['description'].append(discr.text)
+            for price in prices:
+                the_list['price'].append(price.text)
+            the_list['url'].append(i)
+            return the_list
 
         if req.status_code == 400:
             status_code = {400: 'Bad request, try again!'}
@@ -82,6 +68,6 @@ while True:
     URL_TEMPLATE = 'https://aliexpress.ru/wholesale?catId=&SearchText='+input(
         'search_word:')
 
-    df = pd.DataFrame(data=parse(url=URL_TEMPLATE))
-    print(df)
-    # df.to_csv(FILE_NAME)
+    df = url_parse(url=URL_TEMPLATE)
+    df1 = pd.DataFrame(data=parse(url_list=df))
+    print(df1)
